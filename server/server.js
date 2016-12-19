57382272649a8cc220900b5ed20f2a295903a5c2
Hughes89 <mik3h89@gmail.com>
2016-12-19 13:58:21 -0500

    Feat: add server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const port = process.env.PORT || 1337;
+
+app.use(express.static('./build'));
+app.use('/node_modules', express.static('node_modules'));
+
+//require('./config/routes')(app, express);
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
