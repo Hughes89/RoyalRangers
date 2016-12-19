378945d3e1cfeaaa766a3a835e7c23f2389131a0
Hughes89 <mik3h89@gmail.com>
2016-12-19 14:27:53 -0500

    Bug: render index

--- server/server.js
+++ server/server.js
@@ -9,4 +9,8 @@ app.use('/node_modules', express.static('node_modules'));
 
 //require('./config/routes')(app, express);
 
+app.get('/', (req, res, next) => {
+  res.render('index.html');
+});
+
 app.listen(port, () => console.log(`Listening on port: ${port}`));
