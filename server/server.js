2dac7ef2b432cd5d3ebee0c8491dd3bfbc8db22c
Hughes89 <mik3h89@gmail.com>
2016-12-21 14:19:50 -0500

    Feat: add .env variables

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+require('dotenv').config();
 
 const port = process.env.PORT || 1337;
 
@@ -9,4 +10,4 @@ app.use('/node_modules', express.static('node_modules'));
 
 //require('./config/routes')(app, express);
 
-app.listen(port, () => console.log(`Listening on port: ${port}`));
+app.listen(port, () => console.log(`Listening on port: ${port}`, process.env.TEST));
