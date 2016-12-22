0fc1991757cd5b63b4c1665ddf1281a3cd23590e
Hughes89 <mik3h89@gmail.com>
2016-12-22 11:55:37 -0500

    Bug: added get wildcard for

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,8 +1,10 @@
 const about = require('../db/about/aboutController');
 const home = require('../db/home/homeController');
+const path = require('path');
 
 module.exports = (app, express) => {
   app.get('/api/about', about.getAbout);
   app.get('/api/home', home.getHome);
   app.put('/api/home', home.editHome);
+  app.get('*', (req, res, next) => res.sendFile(path.resolve('public/index.html')));
 };
