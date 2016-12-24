e6ee02626dfc84eda31219f507295d63a79c2d25
Hughes89 <mik3h89@gmail.com>
2016-12-24 04:04:18 -0500

    Feat: add 2 way data binding to login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,20 +3,44 @@ import { FlatButton, TextField } from 'material-ui';
 import './Login.css';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  signin(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  handleInput(e, state) {
+    let value = e.target.value;
+    this.setState({
+      [state]: value
+    });
+  }
+
   render() {
     return (
       <div className="Login">
-        <form>
+      <form>
           <TextField
-            defaultValue="Username"
+            hintText="Username"
             floatingLabelText="Username"
+            value={this.state.username}
+            onChange={(e) => this.handleInput(e, 'username')}
             /><br />
           <TextField
-            defaultValue="Password"
+            hintText="Password"
             floatingLabelText="Password"
             type="password"
+            value={this.state.password}
+            onChange={(e) => this.handleInput(e, 'password')}
           /><br />
-          <FlatButton label="Submit" />
+          <FlatButton type="submit" label="Sign in" onClick={(e) => this.signin(e)} />
         </form>
       </div>
     );
