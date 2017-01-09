4aecd55d9120acc5b17ab2f67403831c338ac599
Hughes89 <mik3h89@gmail.com>
2017-01-09 14:05:54 -0500

    Bug: change date to start and end

--- server/db/events/eventsModel.js
+++ server/db/events/eventsModel.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 const EventsSchema = new mongoose.Schema({
   title: String,
-  content: String,
-  date: String
+  description: String,
+  start: String,
+  end: String
 });
 
 module.exports = mongoose.model('Events', EventsSchema);
