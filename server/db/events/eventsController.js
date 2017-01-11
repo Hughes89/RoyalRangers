a095b5e2eabe508832e7c1deaca228e4c1bbd562
Hughes89 <mik3h89@gmail.com>
2017-01-11 11:55:13 -0500

    Feat: add functionality to events controller

---
+++ server/db/events/eventsController.js
@@ -0,0 +1,51 @@
+const Events = require('./eventsModel');
+
+module.exports = {
+  addEvent: (req, res, next) => {
+    if (req.user.privelage === 'user') {
+      return res.sendStatus(401);
+    }
+    Events.create({
+      title: req.body.title,
+      description: req.body.description,
+      start: req.body.start,
+      end: req.body.end })
+      .then(event => {
+        res.sendStatus(201);
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  deleteEvent: (req, res, next) => {
+    console.log(req.body);
+    if (req.user.privelage === 'user') {
+      return res.sendStatus(401);
+    }
+    const id = req.body.id;
+    Events.findById(id)
+      .remove()
+      .exec((err, data) => {
+        if (err) {
+          console.log(err);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(200);
+        }
+      });
+  },
+
+  getEvents: (req, res, next) => {
+    Events.find({})
+      .then(events => {
+        res.json(events);
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(404);
+      })
+  }
+  
+};
