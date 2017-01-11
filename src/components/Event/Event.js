c7bfa764825c7bc01e81b9285975dac0c10f44c2
Hughes89 <mik3h89@gmail.com>
2017-01-11 13:07:37 -0500

    Feat: add time to events modal

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Moment from 'react-moment';
+import { Divider } from 'material-ui';
 
 
 class Event extends React.Component {
@@ -7,6 +9,9 @@ class Event extends React.Component {
     return (
       <div className="Event">
       <p>{event.description}</p>
+      <Divider />
+      <p><strong>Start Date & Time:</strong> <Moment format="MMMM Do YYYY, h:mm a." date={event.start} /></p>
+      <p><strong>End Date & Time</strong>: <Moment format="MMMM Do YYYY, h:mm a." date={event.end} /></p>
       </div>
     );
   }
