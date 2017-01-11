9df84507b99fa4438046119a7cf769a359404684
Hughes89 <mik3h89@gmail.com>
2017-01-11 11:54:55 -0500

    Feat: add event routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,7 @@
 const about = require('../db/about/aboutController');
 const home = require('../db/home/homeController');
 const user = require('../db/users/userController');
+const events = require('../db/events/eventsController');
 const auth = require('./auth');
 const path = require('path');
 
@@ -9,13 +10,18 @@ module.exports = (app, express) => {
   app.get('/api/home', home.getHome);
   app.put('/api/home', auth.isAuth, home.editHome);
 
-  //Authentication
+  //Managing Users
   app.post('/api/signup', auth.isAuth, user.signup);
   app.post('/api/signin', user.signin);
   app.put('/api/password', auth.isAuth, user.changePassword);
   app.get('/api/privelage', auth.isAuth, user.privelageCheck);
   app.get('/api/users', auth.isAuth, user.getAllUsers);
   app.delete('/api/remove/user', auth.isAuth, user.removeUser);
+
+  //Managing Events
+  app.post('/api/add/event', auth.isAuth, events.addEvent);
+  app.get('/api/events', auth.isAuth, events.getEvents);
+  app.delete('/api/remove/event', auth.isAuth, events.deleteEvent);
 
 
   app.get('*', (req, res, next) => res.sendFile(path.resolve('build/index.html')));
