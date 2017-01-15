18c52f442deba570b458862a199fd2882848028b
Hughes89 <mik3h89@gmail.com>
2017-01-15 13:16:48 -0500

    Feat: comment out editabout

--- src/components/Admin/EditAbout/EditAbout.js
+++ src/components/Admin/EditAbout/EditAbout.js
@@ -1,30 +1,30 @@
-import React, { Component } from 'react';
-import { RaisedButton } from 'material-ui';
-import keys from './cloudKeys';
+// import React, { Component } from 'react';
+// import { RaisedButton } from 'material-ui';
+// import keys from './cloudKeys';
 
-import './EditAbout.css';
+// import './EditAbout.css';
 
-class EditAbout extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      body: ''
-    };
-  }
+// class EditAbout extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       body: ''
+//     };
+//   }
 
-  openGalleryWidget() {
-    window.$.cloudinary.config({ cloud_name: keys.cloud_name, api_key: keys.api_key});
-  }
+//   openGalleryWidget() {
+//     window.$.cloudinary.config({ cloud_name: keys.cloud_name, api_key: keys.api_key});
+//   }
 
-  render() {
-    return (
-      <div className="Edit-About">
-        <input name="file" type="file" 
-        className="cloudinary-fileupload" data-cloudinary-field="image_id" 
-        data-form-data="--signedData--" />
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className="Edit-About">
+//         <input name="file" type="file" 
+//         className="cloudinary-fileupload" data-cloudinary-field="image_id" 
+//         data-form-data="--signedData--" />
+//       </div>
+//     );
+//   }
+// }
 
-export default EditAbout;
+// export default EditAbout;
