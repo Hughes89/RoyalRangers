67e861c13a5faeefd5ba4e9be61146fdb0809a6c
Hughes89 <mik3h89@gmail.com>
2017-01-17 12:14:03 -0500

    Feat: only display notes and location if event has them

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -4,12 +4,31 @@ import { Divider } from 'material-ui';
 
 
 class Event extends React.Component {
+  description() {
+    if (this.props.event.description) {
+      return (
+        <p><strong>Notes:</strong> {this.props.event.description}</p>
+      )
+    }
+  }
+
+  location() {
+    if (this.props.event.location) {
+      return (
+        <div>
+        <p><strong>Location:</strong></p>
+        <div dangerouslySetInnerHTML={{__html: this.props.event.location}} />
+        </div>
+      )
+    }
+  }
+
   render() {
     const { event } = this.props;
     return (
       <div className="Event">
-      <p>{event.description}</p>
-      <Divider />
+      {this.description()}
+      {this.location()}
       <p><strong>Start Date & Time:</strong> <Moment format="MMMM Do YYYY, h:mm a." date={event.start} /></p>
       <p><strong>End Date & Time</strong>: <Moment format="MMMM Do YYYY, h:mm a." date={event.end} /></p>
       </div>
