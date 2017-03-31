d1cd197b464a9fc8d4ed2c8fa83eb3dce08ca6c7
Hughes89 <mik3h89@gmail.com>
2017-03-30 23:51:36 -0400

    Feat: change footer to stateless

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './Footer.css';
 
-class Footer extends Component {
-  render() {
-    return (
-      <div className="Footer">
-        <div className="Footer-Center">
-          Copyright 2017 Royal Rangers Outpost 5<br/>
-          Calvary Assembly of God • 4921 Camden Ave, Pennsauken, NJ 08110<br/>
-          Telephone: (856)-665-5808<br/>
-          Website Created By: <a href="https://mhughesiii.herokuapp.com/">Michael Hughes</a>
-        </div>
-      </div>
-    );
-  }
-}
+const Footer = () => (
+  <div className="Footer">
+    <div className="Footer-Center">
+      Copyright 2017 Royal Rangers Outpost 5<br/>
+      Calvary Assembly of God • 4921 Camden Ave, Pennsauken, NJ 08110<br/>
+      Telephone: (856)-665-5808<br/>
+      Website Created By: <a href="https://mhughesiii.herokuapp.com/">Michael Hughes</a>
+    </div>
+  </div>
+);
 
 export default Footer;
