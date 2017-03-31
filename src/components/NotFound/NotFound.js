4a270b8de13034aa2f7890f4b48b44a1de738724
Hughes89 <mik3h89@gmail.com>
2017-03-30 23:53:54 -0400

    Feat: change to stateless component

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './NotFound.css';
 
-class NotFound extends Component {
-  render() {
-    return(
-      <div className="not-found">
-          <h1>404 - Page Not Found</h1>
-          <p>I'm sorry, the page you were looking for cannot be found!</p>
-      </div>
-    )
-  }
-}
+const NotFound = () => (
+  <div className="not-found">
+      <h1>404 - Page Not Found</h1>
+      <p>I'm sorry, the page you were looking for cannot be found!</p>
+  </div>
+)
 
 export default NotFound;
